4ed47ec0400d06ca327d11bf2446515199d11f6c
chenyu-zheng <zcy9998@gmail.com>
2018-06-20 17:39:01 -0500

    Complete basic requirements

---
+++ src/SearchBooks.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ListBooks from './ListBooks';
+
+class SearchBooks extends Component {
+  state = {
+    input: ''
+  };
+
+  handleChange = event => {
+    this.setState({
+      input: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const query = event.target.query.value.trim();
+    query &&
+      this.props.onSubmit(query).then(() =>
+        this.setState({
+          input: query
+        })
+      );
+  };
+
+  setBookStates = () => {
+    const { books, results } = this.props;
+    return results.map(item => {
+      const index = books.findIndex(b => b.id === item.id);
+      if (index > -1) {
+        item.shelf = books[index].shelf;
+      }
+      return item;
+    });
+  };
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                name="query"
+                value={this.state.input}
+                placeholder="Search by title or author"
+                onChange={this.handleChange}
+              />
+            </form>
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ListBooks
+            books={this.setBookStates()}
+            onChangeShelf={this.props.onChangeShelf}
+            onShelfDidChange={() =>
+              this.props.onShelfDidChange(this.state.input)
+            }
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBooks;
